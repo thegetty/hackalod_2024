ccbccba54880c5768a610f77efb89186222672eb
Nabil Kashyap <nkashyap@getty.edu>
2025-03-31 11:44:50 -0700

    add intro text and styles

--- src/src/views/layout-base.vue
+++ src/src/views/layout-base.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="l-base__wrapper">
     <Hero name="Content" :data="heroData" />
+    <div class="l-base__intro gui-container">
+      <RichText :content="markdown" />
+    </div>
     <section>
       <!-- note: container is a getty-ui class that adds the page gutter -->
       <div class="gui-container">
@@ -11,16 +14,19 @@
 </template>
 
 <script>
-import { GettyPage, Hero } from "@thegetty/getty-ui";
+import { Hero, RichText } from "@thegetty/getty-ui";
 import "@thegetty/getty-ui/dist/getty-ui.css";
+import markdownContent from "@/data/intro.md?raw";
+
 export default {
   name: "LayoutBase",
-  components: { Hero },
+  components: { Hero, RichText },
   props: {
     /** * The path the app runs at */ path: { type: String, default: "/" },
   },
   data: function () {
     return {
+      markdown: markdownContent,
       heroData: {
         title: this.$t("base.app_name"),
       },
